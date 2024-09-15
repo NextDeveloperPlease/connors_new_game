09b1f106c5e0bac0511f84fb8f972c92e6b1f507
ConnorNielsen <156475394+ConnorNielsen@users.noreply.github.com>
2024-09-15 11:13:37 -0600

    Added Game_hub (unedited)

--- Scripts/boulder.gd
+++ Scripts/boulder.gd
@@ -10,10 +10,6 @@ func _ready() -> void:
 	clicked = $Boulder_broken
 	modulate = color
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		print('clicked')
